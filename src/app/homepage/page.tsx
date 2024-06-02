dbdc0fa839aa47c119f43209ad0bdea4aa769790
risqiikhsani <risqiikhsani16@gmail.com>
2024-06-02 10:50:50 +0700

    tst

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -3,16 +3,40 @@ import Image from 'next/image'
 export default function Page() {
     return (
         <>
-            <div className="pt-24 pb-8 text-center bg-cyan-400">
-                <Image src="/pictures/3d.svg" width={1000} height={1000} className='w-full h-screen' alt="test" />
-                <p className="text-8xl font-bold">Health4Us</p>
+            <div className="pt-24 pb-8 text-center">
+                <div className="relative w-full h-screen">
+                <Image src="/pictures/3d.svg" layout="fill" objectFit="cover" alt="test" />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                        <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-lg">
+                            <p className="text-8xl font-bold">Health4Us</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='flex justify-center items-center h-screen '>
+
+                    <div>
+                    <Image src="/pictures/girl-with-phone1.png" width={1000} height={1000} alt="test" />
+                    </div>
+                    
+                    <p className="text-8xl font-bold">Ask questions,<br/>help each other,<br/>hangout with people</p>
+                   
+                    
+
+                    
+                </div>
+                <div className='flex justify-center items-center'>
+                    <p className="text-8xl font-bold">Integrated with smart personal assistance</p>
+                    <Image src="/pictures/2.svg" width={1000} height={1000} className='w-fit h-screen' alt="test" />
+                </div>
                 <div className='flex justify-center items-center'>
                     <Image src="/pictures/1.svg" width={1000} height={1000} className='w-full h-screen' alt="test" />
                     <p className="text-8xl font-bold">Integrated with AI powered feature</p>
                 </div>
                 <div className='flex justify-center items-center'>
-                    <p className="text-8xl font-bold">Integrated with smart personal assistance</p>
-                    <Image src="/pictures/2.svg" width={1000} height={1000} className='w-full h-screen' alt="test" />
+                    <p className="text-8xl font-bold">Maintained by professionals</p>
+                    <Image src="/pictures/doctor1.png" width={1000} height={1000} className='w-fit h-fit' alt="test" />
                 </div>
             </div>
         </>
--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 
 import { Bars3Icon, BellAlertIcon, HomeIcon, UserCircleIcon } from "@heroicons/react/16/solid"
 import { Button } from "./ui/button"
@@ -12,10 +14,12 @@ import {
 } from "@/components/ui/sheet"
 import LeftAppBarMobile from "./left-app-bar-mobile"
 import Profile from "./profile"
+import { useAuth } from "@/context/auth"
 
-const LOGIN_URL: any = process.env.NEXT_PUBLIC_COGNITO_LOGIN_URL  
+const LOGIN_URL: any = process.env.NEXT_PUBLIC_COGNITO_LOGIN_URL
 
 export default function AppBar() {
+    const { user } = useAuth()
     return (
 
         <div className="flex items-center justify-between p-4 shadow-md fixed w-full top-0 start-0 border-b border-gray-200 z-40 bg-slate-50">
@@ -28,39 +32,50 @@ export default function AppBar() {
                         </Button>
                     </SheetTrigger>
                     <SheetContent side="left">
-                        <LeftAppBarMobile/>
+                        <LeftAppBarMobile />
                     </SheetContent>
                 </Sheet>
 
-                <Button asChild variant="outline" size="icon">
-                    <Link href="/">
-                        <HomeIcon className="h-6 w-6 text-blue-500" />
-                    </Link>
-                </Button>
+                {user.id && (
+                    <Button asChild variant="outline" size="icon">
+                        <Link href="/">
+                            <HomeIcon className="h-6 w-6 text-blue-500" />
+                        </Link>
+                    </Button>
+                )}
+
             </div>
 
             <div className="flex gap-4">
-            <Button asChild variant="outline">
-                    <Link href={LOGIN_URL}>Login</Link>
-                </Button>
-                <Button variant="outline" size="icon">
-                    <BellAlertIcon className="h-6 w-6 text-blue-500" />
-                </Button>
 
-                <Sheet>
-                    <SheetTrigger asChild>
+
+                {user.id ? (
+                    <>
                         <Button variant="outline" size="icon">
-                            <UserCircleIcon className="h-6 w-6 text-blue-500" />
+                            <BellAlertIcon className="h-6 w-6 text-blue-500" />
                         </Button>
-                    </SheetTrigger>
-                    <SheetContent>
-                        <Profile/>
-                    </SheetContent>
-                </Sheet>
 
-                <Button asChild variant="outline">
-                    <Link href="/logout">Logout</Link>
-                </Button>
+                        <Sheet>
+                            <SheetTrigger asChild>
+                                <Button variant="outline" size="icon">
+                                    <UserCircleIcon className="h-6 w-6 text-blue-500" />
+                                </Button>
+                            </SheetTrigger>
+                            <SheetContent>
+                                <Profile />
+                            </SheetContent>
+                        </Sheet>
+
+                        <Button asChild variant="outline">
+                            <Link href="/logout">Logout</Link>
+                        </Button>
+                    </>
+                ) : (
+                    <Button asChild variant="outline">
+                        <Link href={LOGIN_URL}>Login</Link>
+                    </Button>
+                )}
+
             </div>
 
         </div>
