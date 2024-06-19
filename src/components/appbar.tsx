e5864bb59f0250f3924cdb2b122d5cecd5c94fdd
risqiikhsani <risqiikhsani16@gmail.com>
2024-06-19 18:20:35 +0700

    tst

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -1,4 +1,10 @@
 "use client"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 
 import { Bars3Icon, BellAlertIcon, HomeIcon, UserCircleIcon } from "@heroicons/react/16/solid"
@@ -35,8 +41,8 @@ export default function AppBar() {
 
             {user.id && (
                 <div className="flex gap-4">
-                    <LeftAppBarMobile/>
-                    <Button asChild variant="outline" size="icon" onClick={() => router.refresh()}>
+                    <LeftAppBarMobile />
+                    <Button asChild variant="ghost" size="icon" onClick={() => router.refresh()}>
                         <Link href="/">
                             <Avatar className="mx-auto h-8 w-10 my-20">
                                 <AvatarImage src="/pictures/logo.png" />
@@ -51,21 +57,31 @@ export default function AppBar() {
 
 
             <div className="flex gap-2">
-            <ModeToggle/>
+                <ModeToggle />
 
                 {user.id ? (
                     <>
-                  
-                        
+
+
                         <Sheet>
-                            <SheetTrigger asChild>
-                                <Button variant="outline" size="icon">
-                                    <BellAlertIcon className="h-6 w-6 text-blue-500" />
-                                    <span className="relative flex h-3 w-3">
-                                        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
-                                        <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
-                                    </span>
-                                </Button>
+                            <SheetTrigger>
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger asChild>
+                                            <Button variant="ghost" size="icon">
+                                                <BellAlertIcon className="h-6 w-6" />
+                                                <span className="relative flex h-3 w-3">
+                                                    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+                                                    <span className="relative inline-flex rounded-full h-3 w-3 bg-sky-500"></span>
+                                                </span>
+                                            </Button>
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            <p>Notifications</p>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
+
                             </SheetTrigger >
                             <SheetContent className="overflow-y-auto">
                                 <Notifications />
@@ -73,10 +89,19 @@ export default function AppBar() {
                         </Sheet>
 
                         <Sheet>
-                            <SheetTrigger asChild>
-                                <Button variant="outline" size="icon">
-                                    <UserCircleIcon className="h-6 w-6 text-blue-500" />
-                                </Button>
+                            <SheetTrigger>
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger asChild>
+                                            <Button variant="ghost" size="icon">
+                                                <UserCircleIcon className="h-6 w-6" />
+                                            </Button>
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            <p>Profile</p>
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
                             </SheetTrigger>
                             <SheetContent>
                                 <Profile />
@@ -89,11 +114,11 @@ export default function AppBar() {
                     </>
                 ) : (
                     <>
-                    <Button asChild variant="ghost">
-                        <Link href={LOGIN_URL}>Login</Link>
-                    </Button>
+                        <Button asChild variant="ghost">
+                            <Link href={LOGIN_URL}>Login</Link>
+                        </Button>
                     </>
-                    
+
                 )}
 
             </div>
--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -11,18 +11,35 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
+      <DropdownMenuTrigger>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Theme</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
