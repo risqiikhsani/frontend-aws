a1934d614b915f5d0b924fc71292e04c26267650
risqiikhsani <risqiikhsani16@gmail.com>
2024-06-24 03:56:03 +0700

    added country

--- src/app/(authenticated)/news/_page/filter.tsx
+++ src/app/(authenticated)/news/_page/filter.tsx
@@ -9,6 +9,64 @@ import {
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
+const countries = [
+    { name: "United Arab Emirates", value: "ae" },
+    { name: "Argentina", value: "ar" },
+    { name: "Austria", value: "at" },
+    { name: "Australia", value: "au" },
+    { name: "Belgium", value: "be" },
+    { name: "Bulgaria", value: "bg" },
+    { name: "Brazil", value: "br" },
+    { name: "Canada", value: "ca" },
+    { name: "Switzerland", value: "ch" },
+    { name: "China", value: "cn" },
+    { name: "Colombia", value: "co" },
+    { name: "Cuba", value: "cu" },
+    { name: "Czech Republic", value: "cz" },
+    { name: "Germany", value: "de" },
+    { name: "Egypt", value: "eg" },
+    { name: "France", value: "fr" },
+    { name: "United Kingdom", value: "gb" },
+    { name: "Greece", value: "gr" },
+    { name: "Hong Kong", value: "hk" },
+    { name: "Hungary", value: "hu" },
+    { name: "Indonesia", value: "id" },
+    { name: "Ireland", value: "ie" },
+    { name: "Israel", value: "il" },
+    { name: "India", value: "in" },
+    { name: "Italy", value: "it" },
+    { name: "Japan", value: "jp" },
+    { name: "South Korea", value: "kr" },
+    { name: "Lithuania", value: "lt" },
+    { name: "Latvia", value: "lv" },
+    { name: "Morocco", value: "ma" },
+    { name: "Mexico", value: "mx" },
+    { name: "Malaysia", value: "my" },
+    { name: "Nigeria", value: "ng" },
+    { name: "Netherlands", value: "nl" },
+    { name: "Norway", value: "no" },
+    { name: "New Zealand", value: "nz" },
+    { name: "Philippines", value: "ph" },
+    { name: "Poland", value: "pl" },
+    { name: "Portugal", value: "pt" },
+    { name: "Romania", value: "ro" },
+    { name: "Serbia", value: "rs" },
+    { name: "Russia", value: "ru" },
+    { name: "Saudi Arabia", value: "sa" },
+    { name: "Sweden", value: "se" },
+    { name: "Singapore", value: "sg" },
+    { name: "Slovenia", value: "si" },
+    { name: "Slovakia", value: "sk" },
+    { name: "Thailand", value: "th" },
+    { name: "Turkey", value: "tr" },
+    { name: "Taiwan", value: "tw" },
+    { name: "Ukraine", value: "ua" },
+    { name: "United States", value: "us" },
+    { name: "Venezuela", value: "ve" },
+    { name: "South Africa", value: "za" }
+];
+
+
 export default function Filter() {
     const searchParams = useSearchParams()
     const pathname = usePathname();
@@ -36,8 +94,13 @@ export default function Filter() {
                     <SelectValue placeholder="Country" />
                 </SelectTrigger>
                 <SelectContent>
-                    <SelectItem value="us">US</SelectItem>
-                    <SelectItem value="id">ID</SelectItem>
+                    {
+                        countries.map((country,i) => (
+                            <SelectItem key={i} value={country.value}>{country.name}</SelectItem>
+                        ))
+                    }
+                    {/* <SelectItem value="us">US</SelectItem>
+                    <SelectItem value="id">ID</SelectItem> */}
                 </SelectContent>
             </Select>
         </>
